a9492b44c615a13a83e930cc62208e739746e22d
dilanisawesome <83189859+dilanisawesome@users.noreply.github.com>
2022-07-07 10:58:13 -0700

    first changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Product from "./components/Product";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Vision</h1>
+      <Product />
     </div>
   );
 }
---
+++ src/components/Product.js
@@ -0,0 +1,9 @@
+import "../styles.css";
+
+export default function Product() {
+  return (
+    <div className="App">
+      <h3>Goggles</h3>
+    </div>
+  );
+}
